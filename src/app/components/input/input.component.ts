7c1fe58dd65273bb4c6a048fe5ba91173bbffe15
hossein13m <hossein13mousavi@gmail.com>
2021-06-16 16:54:44 +0430

    chore: initialize input component

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnChanges } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { InputFormConfiguration } from './input.model';
+
+@Component({
+    selector: 'app-input',
+    templateUrl: './input.component.html',
+    styleUrls: ['./input.component.scss'],
+})
+export class InputComponent implements OnChanges {
+    @Input('inputFormConfiguration') inputFormConfiguration: InputFormConfiguration = {
+        formControl: null,
+        type: 'text',
+        label: '',
+        isInputTypePassword: false,
+        color: 'primary',
+        isRtl: false,
+        autofocus: false,
+        readonly: false,
+    };
+
+    public inputMode: 'email' | 'numeric' | 'url' | 'tel' | 'text' = 'text';
+    public hideText = false;
+
+    ngOnChanges(): void {
+        if (!this.FC) throw new Error('FC Input is required');
+        if (this.inputFormConfiguration.isInputTypePassword) this.hideText = true;
+
+        switch (this.inputFormConfiguration.type) {
+            case 'email':
+            case 'tel':
+            case 'url':
+                this.inputMode = this.inputFormConfiguration.type;
+                break;
+            case 'number':
+                this.inputMode = 'numeric';
+                break;
+            default:
+                this.inputMode = 'text';
+        }
+    }
+
+    public get FC(): FormControl {
+        return this.inputFormConfiguration.formControl as FormControl;
+    }
+}
---
+++ src/app/components/input/input.model.ts
@@ -0,0 +1,15 @@
+import { AbstractControl, FormControl } from '@angular/forms';
+import { ThemePalette } from '@angular/material/core';
+
+export interface InputFormConfiguration {
+    formControl: FormControl | AbstractControl | null | undefined;
+    type: 'text' | 'email' | 'tel' | 'url' | 'number' | 'textarea';
+    label: string;
+    placeholder?: string;
+    isInputTypePassword?: boolean;
+    color?: ThemePalette;
+    errorMessages?: [{ errorName: string; errorMessage: string }];
+    isRtl?: boolean;
+    autofocus?: boolean;
+    readonly?: boolean;
+}
