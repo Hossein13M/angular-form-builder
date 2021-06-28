c06b956b92d712de3cba30f012f8ee90e827d574
hossein13m <hossein13mousavi@gmail.com>
2021-06-28 11:46:58 +0430

    feat: configure tailwinds colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     prefix: '',
     purge: {
@@ -5,6 +7,34 @@ module.exports = {
     },
     darkMode: 'class',
     theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            'blue-gray': colors.blueGray,
+            'cool-gray': colors.coolGray,
+            gray: colors.gray,
+            'true-gray': colors.trueGray,
+            'warm-gray': colors.warmGray,
+            red: colors.rose,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            'light-blue': colors.lightBlue,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+        },
         extend: {},
     },
     variants: {
