0d18da4f6992d6e62c17fd7c6f165c8e8a4ad450
hossein13m <hossein13mousavi@gmail.com>
2021-07-03 16:30:54 +0430

    chore: input configuration initialize

---
+++ src/app/components/setting-modal/input-configuration/input-configuration.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputConfigurationComponent } from './input-configuration.component';
+
+@NgModule({
+    declarations: [InputConfigurationComponent],
+    imports: [CommonModule],
+    exports: [InputConfigurationComponent],
+})
+export class InputConfigurationModule {}
