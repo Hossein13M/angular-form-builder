c526f3aafc938c308e61be1e7bb2788def7cda01
hossein13m <hossein13mousavi@gmail.com>
2021-07-11 10:07:12 +0430

    update: implement angular/eslint in project

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,40 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: [],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
+    root: true,
+    ignorePatterns: ['projects/**/*'],
+    overrides: [
+        {
+            files: ['*.ts'],
+            parserOptions: {
+                ecmaVersion: 6,
+                createDefaultProgram: true,
+                project: ['tsconfig.json'],
+            },
+            extends: ['plugin:@angular-eslint/recommended', 'plugin:@angular-eslint/template/process-inline-templates'],
+            rules: {
+                '@angular-eslint/directive-selector': [
+                    'error',
+                    {
+                        type: 'attribute',
+                        prefix: 'app',
+                        style: 'camelCase',
+                    },
+                ],
+                '@angular-eslint/component-selector': [
+                    'error',
+                    {
+                        type: 'element',
+                        prefix: 'app',
+                        style: 'kebab-case',
+                    },
+                ],
+            },
+        },
+        {
+            files: ['*.html'],
+            extends: ['plugin:@angular-eslint/template/recommended'],
+            rules: {},
+        },
+    ],
 };
