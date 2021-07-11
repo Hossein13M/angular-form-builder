4193a93db9893302039b7767c5bcef306265d46c
hossein13m <hossein13mousavi@gmail.com>
2021-07-11 10:55:31 +0430

    chore: implement input configuration component

--- src/app/components/setting-modal/input-configuration/input-configuration.component.ts
+++ src/app/components/setting-modal/input-configuration/input-configuration.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
     selector: 'app-input-configuration',
     templateUrl: './input-configuration.component.html',
     styleUrls: ['./input-configuration.component.scss'],
 })
-export class InputConfigurationComponent {}
+export class InputConfigurationComponent {
+    public themeColors: Array<ThemePalette> = ['primary', 'accent', 'warn'];
+
+    public form: FormGroup = this.fb.group({
+        type: ['text', Validators.required],
+        label: ['', Validators.required],
+        placeholder: [],
+        isInputTypePassword: [false, Validators.required],
+        themeColor: ['primary', Validators.required],
+        isRtl: [false, Validators.required],
+        hint: [],
+    });
+
+    constructor(private fb: FormBuilder) {}
+
+    public submitForm() {
+        console.log(this.form.value);
+    }
+}
