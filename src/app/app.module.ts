c24ed7a0469b539d4285643a43a8e499e0b400ca
Hossein Mousavi <hossein13mousavi@gmail.com>
2021-07-18 23:13:42 +0430

    refactor(app_module): refactor app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-    providers: [],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
