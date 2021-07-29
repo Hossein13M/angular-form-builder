62c104a3ed51afbef960e18f1d6bd65a27559b9d
Hossein Mousavi <hossein13mousavi@gmail.com>
2021-07-29 14:44:02 +0430

    chore(settingModal): initialize save and discard buttons for setting modal

--- src/app/modules/setting-modal/setting-modal.component.ts
+++ src/app/modules/setting-modal/setting-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ColumnModel } from '#models/column.model';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-setting-modal',
@@ -8,6 +9,8 @@ import { ColumnModel } from '#models/column.model';
 })
 export class SettingModalComponent {
     public columns: Array<ColumnModel> = [{ columnId: 1 }];
+
+    constructor(private matDialog: MatDialogRef<SettingModalComponent>) {}
 
     public addColumn(): void {
         this.columns.push({ columnId: 1 });
@@ -20,5 +23,13 @@ export class SettingModalComponent {
 
     public checkColumnsCountForLessThanThree(): boolean {
         return this.columns.length < 3;
+    }
+
+    public saveSectionSetting(): void {
+        this.matDialog.close(true);
+    }
+
+    public cancelSectionSettings(): void {
+        this.matDialog.close(false);
     }
 }
