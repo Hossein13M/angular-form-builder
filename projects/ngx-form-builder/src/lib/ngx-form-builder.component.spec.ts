fcc5d6c5c8577ccf93f6cf14ac30057cbce5e4c6
Hossein Mousavi <hossein13mousavi@gmail.com>
2021-08-15 16:23:02 +0430

    refactor(global): prettify and format ts files

--- projects/ngx-form-builder/src/lib/ngx-form-builder.component.spec.ts
+++ projects/ngx-form-builder/src/lib/ngx-form-builder.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgxFormBuilderComponent } from './ngx-form-builder.component';
 
 describe('NgxFormBuilderComponent', () => {
-  let component: NgxFormBuilderComponent;
-  let fixture: ComponentFixture<NgxFormBuilderComponent>;
+    let component: NgxFormBuilderComponent;
+    let fixture: ComponentFixture<NgxFormBuilderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NgxFormBuilderComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [NgxFormBuilderComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NgxFormBuilderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NgxFormBuilderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- projects/ngx-form-builder/src/lib/ngx-form-builder.service.spec.ts
+++ projects/ngx-form-builder/src/lib/ngx-form-builder.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { NgxFormBuilderService } from './ngx-form-builder.service';
 
 describe('NgxFormBuilderService', () => {
-  let service: NgxFormBuilderService;
+    let service: NgxFormBuilderService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NgxFormBuilderService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(NgxFormBuilderService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
