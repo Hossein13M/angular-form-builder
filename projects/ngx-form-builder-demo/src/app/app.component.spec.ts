8534c6a80b85692ac752b0d35ba0506acf734bc6
Hossein Mousavi <hossein13mousavi@gmail.com>
2021-08-15 20:47:32 +0430

    fix(appComponentSpec): remove title check test case

--- projects/ngx-form-builder-demo/src/app/app.component.spec.ts
+++ projects/ngx-form-builder-demo/src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'ngx-form-builder-demo'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('ngx-form-builder-demo');
-    });
-
     it('should render title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
