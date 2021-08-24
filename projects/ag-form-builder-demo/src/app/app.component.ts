177bb924768868ab9d56ab3fc89cab659cd941d7
Hossein Mousavi <hossein13mousavi@gmail.com>
2021-08-24 17:50:46 +0430

    fix(demo): fix project layout and bring it to the demo application

--- projects/ag-form-builder-demo/src/app/app.component.ts
+++ projects/ag-form-builder-demo/src/app/app.component.ts
@@ -3,7 +3,22 @@ import { Section } from '#models/section.model';
 
 @Component({
     selector: 'app-root',
-    template: `<lib-ag-form-builder (formInfo)="handleFormInfo($event)"></lib-ag-form-builder> `,
+    template: `
+        <div class="demo-wrapper">
+            <lib-ag-form-builder (formInfo)="handleFormInfo($event)"></lib-ag-form-builder>
+        </div>
+    `,
+    styles: [
+        `
+            .demo-wrapper {
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                padding: 3.5rem 0;
+                width: 100%;
+            }
+        `,
+    ],
 })
 export class AppComponent {
     public handleFormInfo(event: { name: string; section: Array<Section> }): void {
