f09ebe5756c22461c7a0d65e3e67e6217e645041
hossein13m <hossein13mousavi@gmail.com>
2021-08-29 10:46:41 +0430

    fix(lib): remove browser animation module from library

--- projects/ag-form-builder/src/lib/ag-form-builder.module.ts
+++ projects/ag-form-builder/src/lib/ag-form-builder.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AgFormBuilderComponent } from './ag-form-builder.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WrapperModule } from './modules/wrapper/wrapper.module';
 
 @NgModule({
     declarations: [AgFormBuilderComponent],
-    imports: [CommonModule, BrowserAnimationsModule, WrapperModule],
+    imports: [CommonModule, WrapperModule],
     exports: [AgFormBuilderComponent],
 })
 export class AgFormBuilderModule {}
