306ca81273199f3495eccd817eb9bbc0bf9e0f8d
hossein13m <hossein13mousavi@gmail.com>
2021-08-29 10:36:31 +0430

    fix(lib): change browser module with common module in library

--- projects/ag-form-builder/src/lib/ag-form-builder.module.ts
+++ projects/ag-form-builder/src/lib/ag-form-builder.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { AgFormBuilderComponent } from './ag-form-builder.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WrapperModule } from './modules/wrapper/wrapper.module';
 
 @NgModule({
     declarations: [AgFormBuilderComponent],
-    imports: [BrowserModule, BrowserAnimationsModule, WrapperModule],
+    imports: [CommonModule, BrowserAnimationsModule, WrapperModule],
     exports: [AgFormBuilderComponent],
 })
 export class AgFormBuilderModule {}
