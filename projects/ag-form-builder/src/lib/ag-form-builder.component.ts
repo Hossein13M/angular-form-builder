4b5c7263c116eb38df2c320a7c59e8d880b96274
Hossein Mousavi <hossein13mousavi@gmail.com>
2021-08-30 11:27:44 +0430

    chore(global): restructure the whole library

--- projects/ag-form-builder/src/lib/ag-form-builder.component.ts
+++ projects/ag-form-builder/src/lib/ag-form-builder.component.ts
@@ -1,5 +1,9 @@
 import { Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { Section } from './models/section.model';
+import { SettingModalComponent } from './components/setting-modal/setting-modal.component';
+import { ColumnComponentModel } from './models/columnComponent.model';
 
 @Component({
     selector: 'ag-form-builder',
@@ -8,8 +12,50 @@ import { Section } from './models/section.model';
     encapsulation: ViewEncapsulation.None,
 })
 export class AgFormBuilderComponent {
-    @Output() emittedForm: EventEmitter<{ name: string; section: Array<Section> }> = new EventEmitter<{ name: string; section: Array<Section> }>();
-    public handleFormInfo(event: { name: string; section: Array<Section> }): void {
-        this.emittedForm.emit(event);
-    }
+  @Output() emittedFormInfo: EventEmitter<{ name: string; sections: Array<Section> }> = new EventEmitter<{ name: string; sections: Array<Section> }>();
+
+  public sections: Array<Section> = [];
+  public form: FormGroup = this.fb.group({
+    name: [null, [Validators.required, Validators.pattern(/^[a-zA-Z]*$/)]],
+  });
+
+  constructor(private dialog: MatDialog, private fb: FormBuilder) {}
+
+  public clearForm(): void {
+    this.sections = [];
+    this.form.reset();
+  }
+
+  public addSection(): void {
+    this.sections.push({ columnsCount: 0 });
+  }
+
+  public removeSection(section: Section): void {
+    const sectionIndex = this.sections.indexOf(section);
+    this.sections.splice(sectionIndex, 1);
+  }
+
+  public openSettingDialog(sectionIndex: number, sectionInfo: Section): void {
+    this.dialog
+      .open(SettingModalComponent, { data: { sectionIndex, sectionInfo }, height: '800px', width: '1600px' })
+      .afterClosed()
+      .subscribe((sectionInfo: { sectionIndex: number; sectionColumns: Array<ColumnComponentModel> }) => {
+        sectionInfo && this.createSectionPreview(sectionInfo);
+      });
+  }
+
+  public submitForm(): void {
+    this.emittedFormInfo.emit({ name: this.form.get('name')!.value, sections: this.sections });
+  }
+
+  public isSubmitButtonDisable(): boolean {
+    return this.form.valid && !!this.sections.length;
+  }
+
+  private createSectionPreview(sectionInfo: { sectionIndex: number; sectionColumns: Array<ColumnComponentModel> }): void {
+    this.sections[sectionInfo.sectionIndex] = {
+      columnInfo: sectionInfo.sectionColumns,
+      columnsCount: sectionInfo.sectionColumns.length,
+    };
+  }
 }
--- projects/ag-form-builder/src/lib/components/setting-modal/setting-modal.module.ts
+++ projects/ag-form-builder/src/lib/components/setting-modal/setting-modal.module.ts
@@ -9,8 +9,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { InputConfigurationModule } from '../../components/input-configuration/input-configuration.module';
-import { ButtonConfigurationModule } from '../../components/button-configuration/button-configuration.module';
+import { InputConfigurationModule } from '../input-configuration/input-configuration.module';
+import { ButtonConfigurationModule } from '../button-configuration/button-configuration.module';
 
 @NgModule({
     declarations: [SettingModalComponent],
