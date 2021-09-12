c79c6f9aba7ceb09a4e17281d25a67e91e526dd8
hossein13m <hossein13mousavi@gmail.com>
2021-09-12 11:47:39 +0430

    feat(models): enhance model interfaces

--- projects/ag-form-builder/src/lib/models/buttonFormConfiguration.ts
+++ projects/ag-form-builder/src/lib/models/buttonFormConfiguration.ts
@@ -1,20 +1,19 @@
-import { AbstractControl, FormControl } from '@angular/forms';
-
 export interface ButtonFormConfiguration {
-    formControlName: AbstractControl | FormControl | null | undefined;
+    formControlName: string | null;
     componentType: 'button';
     type: { name: 'accept' | 'reject' | 'upload' | 'download' | 'localRoute' | 'externalLink'; icon: string };
     themeColor: 'primary' | 'accent' | 'warn';
     label: string;
-    width: string;
+    width?: string;
     isRtl?: boolean;
     tailwindsClassForLabel?: string;
-    hasToolTip: boolean;
+    hasToolTip?: boolean;
     tooltipText?: string;
     tooltipPosition?: 'left' | 'right' | 'above' | 'below' | 'before' | 'after';
     tooltipHideDelay?: number;
-    hasIcon: boolean;
+    hasIcon?: boolean;
     icon?: string;
     localNavigationUrl?: string;
+    downloadOrExternalNavigateLink?: string;
     externalLink?: string;
 }
