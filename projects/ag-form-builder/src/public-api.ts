93d6d5e2505c69e79fa5c8eb71ca3c5742501c04
hossein13m <hossein13mousavi@gmail.com>
2021-09-12 11:57:23 +0430

    feat(lib): change public api due to recent changes

--- projects/ag-form-builder/src/public-api.ts
+++ projects/ag-form-builder/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of ag-form-builder
  */
 
-export * from './lib/ag-form-builder.component';
-export * from './lib/ag-form-builder.module';
+export * from './lib/modules/ag-form-builder/ag-form-builder.component';
+export * from './lib/modules/ag-form-builder/ag-form-builder.module';
