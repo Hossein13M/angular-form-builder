1c9017b94b9b97e5264ed519d944a37d16e96d54
hossein13m <hossein13mousavi@gmail.com>
2021-09-12 11:50:08 +0430

    feat(app-module): import necessary modules and fix routing

--- projects/ag-form-builder-demo/src/app/app.module.ts
+++ projects/ag-form-builder-demo/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AgFormViewerDemoModule } from './modules/ag-form-viewer-demo/ag-form-viewer-demo.module';
 import { AppComponent } from './app.component';
 import { AgFormBuilderModule } from '../../../ag-form-builder/src/lib/ag-form-builder.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
+import { AgFormBuilderDemoModule } from './modules/ag-form-builder-demo/ag-form-builder-demo.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AgFormBuilderModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [BrowserModule, AgFormBuilderModule, BrowserAnimationsModule, AppRoutingModule, AgFormViewerDemoModule, AgFormBuilderDemoModule, RouterModule],
     providers: [],
     bootstrap: [AppComponent],
 })
