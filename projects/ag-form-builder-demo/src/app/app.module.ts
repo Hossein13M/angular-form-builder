11d7d933e8756e587b6e893a6dda82fc5300232a
hossein13m <hossein13mousavi@gmail.com>
2021-09-13 10:38:53 +0430

    refactor(demo)!: new implementation for demo application

--- projects/ag-form-builder-demo/src/app/app.module.ts
+++ projects/ag-form-builder-demo/src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AgFormViewerDemoModule } from './modules/ag-form-viewer-demo/ag-form-viewer-demo.module';
 import { AppComponent } from './app.component';
 import { AgFormBuilderModule } from '../../../ag-form-builder/src/lib/modules/ag-form-builder/ag-form-builder.module';
-import { AgFormBuilderDemoModule } from './modules/ag-form-builder-demo/ag-form-builder-demo.module';
-import { RouterModule } from '@angular/router';
+import { AgFormViewerModule } from '../../../ag-form-builder/src/lib/modules/ag-form-viewer/ag-form-viewer.module';
 
 @NgModule({
     declarations: [AppComponent],
-  imports: [BrowserModule, AgFormBuilderModule, BrowserAnimationsModule, AppRoutingModule, AgFormViewerDemoModule, AgFormBuilderDemoModule, RouterModule],
+    imports: [
+        BrowserModule,
+        AgFormBuilderModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        RouterModule,
+        MatTabsModule,
+        MatIconModule,
+        AgFormViewerModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatTooltipModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
