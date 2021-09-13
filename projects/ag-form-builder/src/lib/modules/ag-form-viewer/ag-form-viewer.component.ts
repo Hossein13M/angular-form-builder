81d77f634f45b355633342d530cf6c935d6aee19
hossein13m <hossein13mousavi@gmail.com>
2021-09-13 16:39:59 +0430

    feat(viewer): initialize formGroup for viewer

--- projects/ag-form-builder/src/lib/modules/ag-form-viewer/ag-form-viewer.component.ts
+++ projects/ag-form-builder/src/lib/modules/ag-form-viewer/ag-form-viewer.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AgFormViewerService } from './ag-form-viewer.service';
+import { Component, Input, OnInit } from '@angular/core';
 import { AgMockFormDataModel } from '../../models/agMockFormData.model';
-import { AgDatepicker } from '../../models/agDatepicker.model';
-import { defaultDatepickerConfiguration } from '../../const/defaultDatepickerConfiguration';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AgColumnComponentModel } from '../../models/agColumnComponent.model';
+import { AgButtonFormConfiguration } from '../../models/agButtonFormConfiguration';
+import { AgInputFormConfiguration } from '../../models/agInputFormConfiguration.model';
+import { AgDatepickerConfiguration } from '../../models/agDatepickerConfiguration.model';
 
 @Component({
     selector: 'lib-ag-form-viewer',
@@ -10,14 +12,44 @@ import { defaultDatepickerConfiguration } from '../../const/defaultDatepickerCon
     styleUrls: ['./ag-form-viewer.component.scss'],
 })
 export class AgFormViewerComponent implements OnInit {
-    public mockForm!: AgMockFormDataModel;
-    public defaultDatePicker: AgDatepicker = defaultDatepickerConfiguration;
-    constructor(private readonly agFormViewerService: AgFormViewerService) {}
+    @Input() formInfo!: AgMockFormDataModel;
+    public form: FormGroup = this.fb.group({
+        name: ['', Validators.required],
+        formArray: this.fb.array([]),
+    });
+    public formArray: FormArray = this.fb.array([]);
 
-    ngOnInit(): void {
-        this.agFormViewerService.getMockData().subscribe((result) => {
-            this.mockForm = result;
-            console.log(result);
+    constructor(private readonly fb: FormBuilder) {}
+
+    private createFormGroups(): void {
+        const dastan: any[] = [];
+        this.form.get('name')?.setValue(this.formInfo.name);
+        this.formInfo.sections.map((sectionInfo) => {
+            let fg: FormGroup = this.fb.group({});
+            sectionInfo?.columnInfo?.map((columnInfo) => {
+                fg.addControl(`${columnInfo.componentInfo.formControlName}`, new FormControl('dastan', Validators.required));
+            });
+            dastan.push(fg);
         });
+        this.form.get('formArray')?.setValue(dastan);
+        console.log(this.formArray);
+    }
+
+    public ngOnInit(): void {
+        this.createFormGroups();
+    }
+
+    public onSubmit(): void {}
+
+    public returnButtonComponentConfiguration(component: AgColumnComponentModel): AgButtonFormConfiguration {
+        return <AgButtonFormConfiguration>component.componentInfo;
+    }
+
+    public returnInputComponentConfiguration(component: AgColumnComponentModel): AgInputFormConfiguration {
+        return <AgInputFormConfiguration>component.componentInfo;
+    }
+
+    public returnDatepickerComponentConfiguration(component: AgColumnComponentModel): AgDatepickerConfiguration {
+        return <AgDatepickerConfiguration>component.componentInfo;
     }
 }
