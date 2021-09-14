08bf1c9feb56871dfed706daba79b44b3528cdb1
hossein13m <hossein13mousavi@gmail.com>
2021-09-14 16:04:51 +0430

    feat(viewer): implement formViewer

--- projects/ag-form-builder/src/lib/modules/ag-form-viewer/ag-form-viewer.component.ts
+++ projects/ag-form-builder/src/lib/modules/ag-form-viewer/ag-form-viewer.component.ts
@@ -1,10 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AgMockFormDataModel } from '../../models/agMockFormData.model';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { AgColumnComponentModel } from '../../models/agColumnComponent.model';
-import { AgButtonFormConfiguration } from '../../models/agButtonFormConfiguration';
-import { AgInputFormConfiguration } from '../../models/agInputFormConfiguration.model';
-import { AgDatepickerConfiguration } from '../../models/agDatepickerConfiguration.model';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { formatDate } from '@angular/common';
 
 @Component({
     selector: 'lib-ag-form-viewer',
@@ -13,43 +10,69 @@ import { AgDatepickerConfiguration } from '../../models/agDatepickerConfiguratio
 })
 export class AgFormViewerComponent implements OnInit {
     @Input() formInfo!: AgMockFormDataModel;
-    public form: FormGroup = this.fb.group({
-        name: ['', Validators.required],
-        formArray: this.fb.array([]),
-    });
+    public form: FormGroup;
     public formArray: FormArray = this.fb.array([]);
+    public hasFormCreated: boolean = false;
 
-    constructor(private readonly fb: FormBuilder) {}
-
-    private createFormGroups(): void {
-        const dastan: any[] = [];
-        this.form.get('name')?.setValue(this.formInfo.name);
-        this.formInfo.sections.map((sectionInfo) => {
-            let fg: FormGroup = this.fb.group({});
-            sectionInfo?.columnInfo?.map((columnInfo) => {
-                fg.addControl(`${columnInfo.componentInfo.formControlName}`, new FormControl('dastan', Validators.required));
-            });
-            dastan.push(fg);
-        });
-        this.form.get('formArray')?.setValue(dastan);
-        console.log(this.formArray);
+    constructor(private readonly fb: FormBuilder) {
+        this.form = this.fb.group({ name: ['', Validators.required], formArray: [] });
     }
 
     public ngOnInit(): void {
         this.createFormGroups();
     }
 
-    public onSubmit(): void {}
-
-    public returnButtonComponentConfiguration(component: AgColumnComponentModel): AgButtonFormConfiguration {
-        return <AgButtonFormConfiguration>component.componentInfo;
+    get getFormArray(): FormArray {
+        return this.form.controls.formArray as FormArray;
     }
 
-    public returnInputComponentConfiguration(component: AgColumnComponentModel): AgInputFormConfiguration {
-        return <AgInputFormConfiguration>component.componentInfo;
+    private createFormGroups(): void {
+        this.form.get('name')?.setValue(this.formInfo.name);
+        this.formInfo.sections.map((sectionInfo) => {
+            const formGroup: FormGroup = this.fb.group({});
+            sectionInfo?.columnInfo?.map((columnInfo) => {
+                formGroup.addControl(columnInfo.componentInfo.formControlName ?? '', new FormControl(null, Validators.required));
+            });
+            this.formArray.push(formGroup);
+        });
+        this.form.get('formArray')?.setValue(this.formArray);
+        this.hasFormCreated = true;
     }
 
-    public returnDatepickerComponentConfiguration(component: AgColumnComponentModel): AgDatepickerConfiguration {
-        return <AgDatepickerConfiguration>component.componentInfo;
+    public onSubmit(): void {
+        this.form.get('formArray')?.value.value.map((item: any) => {
+            for (const key in item) {
+                if (AgFormViewerComponent.isDate(item[key])) {
+                    item[key] = AgFormViewerComponent.convertDateToGregorianFormatForServer(item[key]);
+                }
+            }
+        });
+
+        const dastan = { ...this.form.get('formArray')?.value.value, name: this.form.get('name')?.value };
+        console.log(dastan);
+    }
+
+    public returnFormGroupControls(formGroup: any) {
+        const formControlNames: string[] = [];
+        Object.keys(formGroup.controls).map((formControl) => formControlNames.push(formControl));
+        return formControlNames;
+    }
+
+    public returnFormControlName(index: number, formControlName: string) {
+        return this.formInfo?.sections[index]?.columnInfo?.find((item) => {
+            return item.componentInfo.formControlName === formControlName;
+        });
+    }
+
+    public getFormGroup(form: AbstractControl) {
+        return form as FormGroup;
+    }
+
+    private static convertDateToGregorianFormatForServer(date: Date): string {
+        return formatDate(date, 'yyyy-MM-dd', 'en_US');
+    }
+
+    private static isDate(dateStr: string): boolean {
+        return !isNaN(new Date(dateStr).getDate()) && dateStr !== null;
     }
 }
